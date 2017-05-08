769b5df80e04305b2a93678e26c11a5a132a6da8
Dmitrii <dmitriitkrsk@gmail.com>
2017-05-08 23:33:36 +0700

    Add Views

--- TestFilm/Controllers/HomeController.cs
+++ TestFilm/Controllers/HomeController.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TestFilm.Models;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authorization;
 
 namespace TestFilm.Controllers
@@ -10,24 +14,50 @@ namespace TestFilm.Controllers
         //{
         //    return Content("Добро пожаловать" + User.Identity.Name);
         //}
+        
+        private FilmContext db;
+        public HomeController(FilmContext context)
+        {
+            db = context;
+        }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
+        {
+            return View(await db.Films.ToListAsync());
+        }
+        [Authorize]
+        public IActionResult Create()
         {
             return View();
         }
-
-        public IActionResult About()
+        
+        [HttpPost]
+        public async Task<IActionResult> Create(Film film)
         {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
+            film.User = User.Identity.Name;
+            db.Films.Add(film);                        
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
         }
 
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
 
-            return View();
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id != null)
+            {
+                Film film = await db.Films.FirstOrDefaultAsync(p => p.Id == id);
+                if (film != null)
+                    return View(film);
+            }
+            return NotFound();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(Film film)
+        {
+            film.User = User.Identity.Name;
+            db.Films.Update(film);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
         }
 
         public IActionResult Error()
--- TestFilm/Models/Film.cs
+++ TestFilm/Models/Film.cs
@@ -1,10 +1,16 @@
-﻿public class Film
+﻿using System;
+namespace TestFilm.Models
 {
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string Description { get; set; }
-    public int Year { get; set; }
-    public string Director { get; set; }
-    public byte[] Image { get; set; }
-    public string User { get; set; }
+    public class Film
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Year { get; set; }
+        public string Director { get; set; }
+        public byte[] Image { get; set; }
+        public string User { get; set; }
+    }
+
+   
 }
